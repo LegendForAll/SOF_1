3e05223a2d54fc8615ef48dcfe01aa42f2373899
HaDangTu <38284966+HaDangTu@users.noreply.github.com>
2019-03-20 18:55:46 +0700

    Add files via upload

---
+++ QLCAFE/QLCAFE/Login.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using QLCAFE_VM;
+
+namespace QLCAFE
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+            this.Closing += new CancelEventHandler(Window_closing);
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            LoginVM loginVM = new LoginVM();
+            if (loginVM.Login(tbUserName.Text, pbPassword.Password))
+            {
+                MessageBox.Show("Hello " + tbUserName.Text, "Infomation", MessageBoxButton.OK, MessageBoxImage.Information);
+                Closing -= new CancelEventHandler(Window_closing);
+                Close();                
+            }               
+            else
+                MessageBox.Show("Sai ten username hoac password", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void Window_closing(object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;
+        }       
+    }
+}
---
+++ QLCAFE/QLCAFE_VM/LoginVM.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using QLCAFE_Model;
+
+namespace QLCAFE_VM
+{
+    public class LoginVM
+    {
+        private string connectionString = "Data Source=DESKTOP-H75CAQB;Initial Catalog=QLCAFE;Integrated Security=True";
+
+        public bool Login(string userName, string password)
+        {
+            bool result = false;
+            string query = "SELECT * FROM NhanVien WHERE [TenDangNhap] = @TenDangNhap";
+            Login login = null;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = query;
+                    command.Parameters.AddWithValue("@TenDangNhap", userName);
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader;
+                        reader = command.ExecuteReader();
+                        if (reader.HasRows == true)
+                            while (reader.Read())
+                                login = new Login(reader["MaNV"].ToString(), reader["HoTen"].ToString(),
+                                    reader["TenDangNhap"].ToString(), reader["MatKhau"].ToString());
+                    
+
+                        if (login != null)
+                        {
+                            if (login.Password.Equals(password) && login.UserName.Equals(userName))
+                                result = true;
+                            else
+                                result = false;
+                        }
+                        return result;
+
+                    }
+                    catch(Exception e)
+                    {
+                        connection.Close();
+                        System.Console.WriteLine(e.Message);
+                        return false;
+                    }
+                }
+            } 
+        }
+    }
+}
